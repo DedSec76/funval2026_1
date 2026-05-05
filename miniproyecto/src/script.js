e6a8280d34d8fbc32347a380caedf788ff32e3fe
Aldair Rutte <xxpiltisxx@gmail.com>
2026-05-05 18:54:31 -0500

    Version 2 de la web. se arreglan y se crearon nuevos apartados

--- miniproyecto/src/script.js
+++ miniproyecto/src/script.js
@@ -1,7 +1,14 @@
 const nav = document.querySelector("#menu")
 const btnHamburger = document.querySelector("#btnMenu")
+const lastModified = document.getElementById("lastModified")
+const year = document.getElementById("year")
+
+const date = new Date()
 
 btnHamburger.addEventListener("click", () => {
     nav.dataset.open = nav.dataset.open === "true" ? "false" : "true";
     btnHamburger.classList.toggle("open");
 })
+
+year.textContent = date ? date.getFullYear() : "none"
+lastModified.textContent = `Last Modified: ${document.lastModified}`
