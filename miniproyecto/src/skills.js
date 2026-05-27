8311490f5f51832a04215e4367061868e3d3b06c
Aldair Rutte <xxpiltisxx@gmail.com>
2026-05-26 23:26:27 -0500

    Se añadio un objeto con la informacion de mis habilidades

---
+++ miniproyecto/src/skills.js
@@ -0,0 +1,31 @@
+export const aldairRB = {
+    name: "Aldair Rutte Bazán",
+    nivelBackend: 60,
+    nivelFrontend: 80,
+    reviews: "Working with Aldair was an amazing experience. He delivered a clean, fast, and professional fullstack solution exactly as requested. Communication was excellent, and the final product exceeded expectations.",
+    description: 
+    `I Am a full-stack website developer specialized in React.JS. Put I also know some Wordpress front end development with WooCommerce.
+    Programming is my passion, that's the reason I am working at the time of my academic career.
+    Additionally, I like to play PC games and read story-type books in my free time within is kinda fun and relaxing!`,
+    skills: ["HTML", "CSS", "JS", "React", "Next.js", "Node.js", "Express", "Tailwind", "MySQL", "PostgreSQL", "MongoDB"],
+    additionalSkills: ["Python", "C#", "Java", "PHP", "Sass", "Git", "GitHub", "pnpm", "PowerBI", "Excel", "Swagger", "Zod-Validator"], 
+    projects: [
+        {
+            name: 'Tienda de Postres',
+            imgSrc: '/imgProjects/desserts.webp'
+        },
+        {
+            name: 'Admin Dashboard',
+            imgSrc: '/imgProjects/dashboardDev.webp'
+        },
+        {
+            name: 'Comparador de Precio',
+            imgSrc: '/imgProjects/comparePrice.webp'
+        },
+        {
+            name: 'Tienda de Playeras',
+            imgSrc: '/imgProjects/store.webp'
+        }
+    ],
+    imgProfile: ["/myProfile.webp", "/sitTable.webp", "/standFront.webp", "/casualClothe.webp"],
+}
